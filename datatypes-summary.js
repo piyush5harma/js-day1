c8b285cf2c40dfb0be2ce9f6e54307c8d15db258
Piyush <pk0012565@gmail.com>
2025-08-21 18:25:50 +0530

    Create datatypes-summary.js
    data type summary

---
+++ datatypes-summary.js
@@ -0,0 +1,17 @@
+// primitive data types
+let name = "pihu" // string
+let age = 76      // number
+let isAlive = true // boolean
+let education = null //null/object
+let homework         //undefined
+let id = symbol('ab12')  //symbol
+
+// non primitive/reference data types
+let vehicles = ["car","bike","cycle","boat"] //array
+let myObj = {
+    weight : 82,
+    height : 6.2,
+} //object
+let myFunction = function(){
+    console.log("1+1=2")
+} //function
