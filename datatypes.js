1d9af7f18d11640a9ce759ed8d65fd3208a4bb8f
Piyush <pk0012565@gmail.com>
2025-08-21 16:41:17 +0530

    Create datatypes.js
    learnt datatypes

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+let name = "pranav"
+
+console.log(typeof name)
+
+let age = 25
+
+console.log(typeof age)
+
+let employment = true
+
+console.log(typeof employment)
+
+let girlfriend = null
+
+console.log(typeof girlfriend)
+
+let friends = undefined
+
+console.log(typeof friends)
