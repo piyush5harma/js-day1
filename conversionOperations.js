4ad0a94fb1737974e3b5957bcdd07e2309420833
Piyush <pk0012565@gmail.com>
2025-08-21 17:00:07 +0530

    Create conversionOperations.js
    learnt conversions operation

---
+++ conversionOperations.js
@@ -0,0 +1,21 @@
+// string to number
+let age = "33"
+console.log(typeof age)
+let NumAge = Number(age)
+console.log(typeof NumAge)
+console.log(NumAge)
+
+// number to string
+let score = 33
+console.log(typeof score)
+let StringScore = String(score)
+console.log(typeof StringScore)
+console.log(StringScore)
+
+// boolean to number 
+let isAdult = true
+console.log(typeof isAdult)
+let NumisAdult = Number(isAdult)
+console.log(typeof NumisAdult)
+console.log(NumisAdult)
+
