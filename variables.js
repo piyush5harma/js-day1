25e7e49e38ecdc31d9767d85eb209e1cd4808536
Piyush <pk0012565@gmail.com>
2025-08-21 16:16:14 +0530

    Create variables.js
    learned variables in js

---
+++ variables.js
@@ -0,0 +1,15 @@
+const AccountId = 123
+let accountEmail = "hello@gmail.com"
+var AccountPassword = "#@$%"
+AccountCity = "kolkata"
+
+// AccountId = 323
+
+accountEmail = "pok@gmail.com"
+AccountPassword = "$124$%"
+AccountCity = "karachi"
+let accountState
+
+
+console.log(AccountId)
+console.table([accountEmail, AccountId, AccountPassword, AccountCity, accountState])
