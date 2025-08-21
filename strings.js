04f35323a0a5e9bc26a52299d07607d641045c84
Piyush <pk0012565@gmail.com>
2025-08-21 19:47:32 +0530

    Create strings.js
    strings

---
+++ strings.js
@@ -0,0 +1,12 @@
+let FirstName = "pankaj"
+let LastName = " tripathi"
+let age = 55
+
+console.log(`hello my name is ${FirstName}${LastName} and my age is ${age}`)
+
+let UserId = new String('Pankaj123') 
+console.log(UserId.length);
+console.log(UserId.charAt(8));
+console.log(UserId.indexOf('a'));
+// console.log(UserId.replace(Pan,Man));
+
